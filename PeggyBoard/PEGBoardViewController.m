14234945b818aeea6eea586807d1ec0a9b3514a4
Jake Good <jake@developstuff.com>
2014-04-01 15:21:20 -0500

    Moves clear to the backend thread

--- PeggyBoard/PEGBoardViewController.m
+++ PeggyBoard/PEGBoardViewController.m
@@ -113,7 +113,9 @@
 
 - (void) clearBoard {
     [self.board clear];
-    [[PEGClient sharedClient] clear:0];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [[PEGClient sharedClient] clear:0];
+    });
     [self.view setNeedsDisplay];
 }
 
